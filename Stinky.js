da8c4704aedb01f9bce1e92c1e98707c3dfa491c
Dylan Ystebo <33172933+DYstebo@users.noreply.github.com>
2020-08-02 15:09:14 -0500

    added new reaction

--- Stinky.js
+++ Stinky.js
@@ -10,7 +10,8 @@ client.on('message', message => {
     sentMessage = message.toString().toLowerCase();
     if(sentMessage.includes("gorney's grill") || sentMessage.includes("stinky") || message.content.includes("734564767741771928") || message.content.includes("737102276170350692")) {
         var stinky = "739315404757008484";
-        message.react(stinky);
+        message.react('💩');
+	message.react(stinky);
     }
     if(sentMessage.includes("ape") || sentMessage.includes("monkey")  || sentMessage.includes("monke") || sentMessage.includes("chimp") || message.content.includes("545994019088367636") || message.content.includes("673224954023510037") || message.content.includes("673736495885975552")) {
         var ooga = "735920050749440081";
