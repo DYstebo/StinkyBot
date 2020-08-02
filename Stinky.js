6cbc1667d45ca40b2b8744689780bde724b83305
Dylan Ystebo <33172933+DYstebo@users.noreply.github.com>
2020-08-02 14:57:41 -0500

    Add files via upload

--- Stinky.js
+++ Stinky.js
@@ -8,10 +8,15 @@ client.once('ready', () => {
 
 client.on('message', message => {
     sentMessage = message.toString().toLowerCase();
-    if(sentMessage.includes("gorney's grill") || sentMessage.includes("stinky")) {
+    if(sentMessage.includes("gorney's grill") || sentMessage.includes("stinky") || message.content.includes("734564767741771928") || message.content.includes("737102276170350692")) {
         var stinky = "739315404757008484";
         message.react(stinky);
     }
+    if(sentMessage.includes("ape") || sentMessage.includes("monkey")  || sentMessage.includes("monke") || sentMessage.includes("chimp") || message.content.includes("545994019088367636") || message.content.includes("673224954023510037") || message.content.includes("673736495885975552")) {
+        var ooga = "735920050749440081";
+        var chimp = '🐒';
+        message.react(ooga);
+        message.react(chimp);
+    }
 });
-
 client.login(config.botToken);
